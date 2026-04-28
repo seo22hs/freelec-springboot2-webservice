c7430e8508f9b3583300d70b8c416fa472cd0d5c
seo22hs <lsc584727@gmail.com>
2026-04-28 15:09:48 +0900

    Day02-practice - feat : PostsService.findAllDesc() 추가

--- src/main/java/com/jojoldu/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/jojoldu/book/springboot/service/posts/PostsService.java
@@ -9,6 +9,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RequiredArgsConstructor
 @Service
 
@@ -42,5 +45,13 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException(
                         "해당 게시글이 없습니다. id=" + id));
         postsRepository.delete(posts);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsResponseDto> findAllDesc() {
+        return postsRepository.findAll()                  // (1)
+                .stream()
+                .map(PostsResponseDto::new)               // (2)
+                .collect(Collectors.toList());
     }
 }
