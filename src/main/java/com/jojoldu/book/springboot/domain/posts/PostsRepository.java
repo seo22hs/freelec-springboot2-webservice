65e5bb7b4e00f9c8191124dc109c3e41ddc5cb2f
seo22hs <lsc584727@gmail.com>
2026-04-28 15:57:30 +0900

    Day02-practice - feat: PostsRepository.findByAuthor 쿼리 메서드 추가

--- src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
@@ -1,7 +1,8 @@
 package com.jojoldu.book.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
-
+    List<Posts> findByAuthor(String author);
 }
