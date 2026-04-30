98171b2c2669efaae127e78e7c0c6e9cacf4a0c6
seo22hs <lsc584727@gmail.com>
2026-04-30 14:55:34 +0900

    docs : findAllDesc() 오류 수정

--- src/main/java/com/jojoldu/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/jojoldu/book/springboot/service/posts/PostsService.java
@@ -9,6 +9,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RequiredArgsConstructor
 @Service
 
@@ -28,6 +31,14 @@ public class PostsService {
         return id;
     }
 
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException(
+                        "해당 게시글이 없습니다. id=" + id));
+        postsRepository.delete(posts);
+    }
+
     @Transactional(readOnly = true)
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id)
@@ -36,11 +47,10 @@ public class PostsService {
         return new PostsResponseDto(entity);
     }
 
-    @Transactional
-    public void delete(Long id) {
-        Posts posts = postsRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException(
-                        "해당 게시글이 없습니다. id=" + id));
-        postsRepository.delete(posts);
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findALLDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
